8b43901bc07f39270df093b683e963c7dd5877c0
Christopher Cortes <christopherctes@hotmail.com>
2024-01-26 18:14:54 -0300

    week 3 activity

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,13 +1,3 @@
-// const utilities = require('../utilities/')
-// const baseController = {}
-
-// baseController.buildHome = async function (req, res) {
-//     const nav = await utilities.getNav()
-//     res.render('index', {title: 'Home', nav})
-// }
-
-// module.exports = baseController
-
 const utilities = require("../utilities/");
 const baseController = {};
 
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,13 +1,3 @@
-// //Needed Resources
-// const express = require('express')
-// const router = new express.Router()
-// const invController = require('../controllers/invController')
-
-// //Route to build inventory by classification view
-// router.get('/type/:classificationId', invController.buildByClassificationId);
-
-// module.exports = router;
-
 // Needed Resources
 const express = require("express");
 const router = new express.Router();
